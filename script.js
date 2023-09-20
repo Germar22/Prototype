0ec114a0e78d6a5b32bf985f13fdd2d586c6e40b
Germar Bunda <123885878+Germar22@users.noreply.github.com>
2023-09-20 11:23:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll(".navbar > nav > ul > li ");
+const cards = document.querySelectorAll(".card");
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13); // Set initial coordinates and zoom level
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+[...links].map((link, index) =>{
+    link.addEventListener("click", () => onLinkClick(link, index), false);
+});
+
+const onLinkClick = (link, currentIndex) => {
+    const selectedItem = link.getAttribute("name")
+    cards.forEach(card => {
+        card.classList.remove("active");
+    });
+    const currentCard = [...cards].find(card => card.classList.contains(selectedItem)
+    );
+    currentCard.classList.add("active");
+    highLightSelectedLink(currentIndex);
+};
+
+const highLightSelectedLink = (currentIndex) => {
+    links.forEach((link) => {
+        link.classList.remove("selectedLink");
+    });
+    links[currentIndex].classList.add("selectedLink");
+}
+
