7292fabe8b507ce1559d13306fade9cc248158f9
Roger Anthony D. Bairoy <rogeranthony1127@gmail.com>
2023-10-07 19:44:09 +0800

    Added Charts
    Used Chart.js to add charts to the prototype

---
+++ charts.js
@@ -0,0 +1,936 @@
+// Get a reference to the first canvas element
+var ctx1 = document.getElementById("chart1").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Apopong",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart1 = new Chart(ctx1, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx2 = document.getElementById("chart2").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Bula",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart2 = new Chart(ctx2, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx3 = document.getElementById("chart3").getContext("2d");
+
+// Define data and options for the second chart
+var data3 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Bawing",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart3 = new Chart(ctx3, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx4 = document.getElementById("chart4").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Buayan",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart4 = new Chart(ctx4, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx5 = document.getElementById("chart5").getContext("2d");
+
+// Define data and options for the second chart
+var data5 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Calumpang",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart5 = new Chart(ctx5, {
+  type: "line", // Replace with the desired chart type
+  data: data5,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx6 = document.getElementById("chart6").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "City Heights",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart6 = new Chart(ctx6, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx7 = document.getElementById("chart7").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Conel",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart7 = new Chart(ctx7, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx8 = document.getElementById("chart8").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Dadiangas East",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart8 = new Chart(ctx8, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx9 = document.getElementById("chart9").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Dadiangas North",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart9 = new Chart(ctx9, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx10 = document.getElementById("chart10").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Dadiangas South",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart10 = new Chart(ctx10, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx11 = document.getElementById("chart11").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Dadiangas West",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart11 = new Chart(ctx11, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx12 = document.getElementById("chart12").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Fatima",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart12 = new Chart(ctx12, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx13 = document.getElementById("chart13").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Labangal",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart13 = new Chart(ctx13, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx14 = document.getElementById("chart14").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Labangal",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart14 = new Chart(ctx14, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx15 = document.getElementById("chart15").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Lagao",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart15 = new Chart(ctx15, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx16 = document.getElementById("chart16").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Ligaya",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart16 = new Chart(ctx16, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx17 = document.getElementById("chart17").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Mabuhay",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart17 = new Chart(ctx17, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx18 = document.getElementById("chart18").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Magsaysay",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart18 = new Chart(ctx18, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx19 = document.getElementById("chart19").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Olympog",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart19 = new Chart(ctx19, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx20 = document.getElementById("chart20").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "San Isidro",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart20 = new Chart(ctx20, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx21 = document.getElementById("chart21").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "San Jose",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart21 = new Chart(ctx21, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx22 = document.getElementById("chart22").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Siguel",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart22 = new Chart(ctx22, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx23 = document.getElementById("chart23").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Sinawal",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart23 = new Chart(ctx23, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
+// Get a reference to the first canvas element
+var ctx24 = document.getElementById("chart24").getContext("2d");
+
+// Define data and options for the first chart
+var data1 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Tambler",
+      data: [3, 1, 4, 1, 3, 2, 4, 3, 2, 1, 2, 1],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+var options = {
+  backgroundColor: "white",
+};
+
+// Create the first chart
+var chart24 = new Chart(ctx24, {
+  type: "line", // Replace with the desired chart type
+  data: data1,
+  options: options,
+});
+
+// Get a reference to the second canvas element
+var ctx25 = document.getElementById("chart25").getContext("2d");
+
+// Define data and options for the second chart
+var data2 = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Tinagacan",
+      data: [2, 3, 1, 1, 3, 2, 4, 2, 1, 2, 4, 2],
+      backgroundColor: ["black"],
+      lineTension: 0.8,
+    },
+  ],
+};
+
+// Create the second chart
+var chart25 = new Chart(ctx25, {
+  type: "line", // Replace with the desired chart type
+  data: data2,
+  options: options,
+});
--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-var map = L.map('map');
+var map = L.map("map");
 map.setView([6.0717, 125.1217], 16);
 
-const links=document.querySelectorAll(".navbar > nav > ul > li");
-const cards=document.querySelectorAll(".card");
+const links = document.querySelectorAll(".navbar > nav > ul > li");
+const cards = document.querySelectorAll(".card");
 
 // Add OpenStreetMap tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var marker = L.marker([6.06940, 125.12620]).addTo(map);
+var marker = L.marker([6.0694, 125.1262]).addTo(map);
 
-var marker = L.marker([6.07449, 125.11460]).addTo(map);
+var marker = L.marker([6.07449, 125.1146]).addTo(map);
 
 var marker = L.marker([6.0678, 125.1195]).addTo(map);
 
-
-[...links].map((link,index) => {
-    link.addEventListener("click", () => onLinkClick(link, index), false);
+[...links].map((link, index) => {
+  link.addEventListener("click", () => onLinkClick(link, index), false);
 });
 
 const onLinkClick = (link, currentIndex) => {
-    const selectedItem = link.getAttribute("name")
-    cards.forEach(card => {
-        card.classList.remove("active");
-    });
-    const currentCard = [...cards].find(card => card.classList.contains(selectedItem)
-    );
-    currentCard.classList.add("active");
-    highLightSelectedLink(currentIndex);
+  const selectedItem = link.getAttribute("name");
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+  const currentCard = [...cards].find((card) =>
+    card.classList.contains(selectedItem)
+  );
+  currentCard.classList.add("active");
+  highLightSelectedLink(currentIndex);
 };
 
 const highLightSelectedLink = (currentIndex) => {
-    links.forEach((link) => {
-        link.classList.remove("selectedLink");
-    });
-    links[currentIndex].classList.add("selectedLink");
+  links.forEach((link) => {
+    link.classList.remove("selectedLink");
+  });
+  links[currentIndex].classList.add("selectedLink");
 };
